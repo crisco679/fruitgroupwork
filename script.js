971c2bc68c100865d8f92c4ccb1739e69ff75bf6
christuttle <crunch.tuttle@gmail.com>
2016-03-29 18:19:02 -0500

    First Commt

---
+++ script.js
@@ -0,0 +1,25 @@
+$(function(){
+
+var costApple =70;
+var costBanana =85;
+var costOrange =65;
+var costGrape =55;
+
+
+
+	// console.log(costApple);
+	// costBanana = costBanana + (Math.floor(Math.random() * (1 + max - min) + min));
+	// console.log(costBanana);
+	// costOrange = costOrange + (Math.floor(Math.random() * (1 + max - min) + min));
+	// costGrape = costGrape + (Math.floor(Math.random() * (1 + max - min) + min));
+
+
+});
+//Put only whole numbers in randomNumber(-50,50)
+randomNumber(.01, .5);
+function randomNumber(min, max) {
+	return Math.floor(Math.random() * (1 + max - min) + min);
+}
+//figure out how to make it work once
+
+//figure out how to put it into setInterval function
